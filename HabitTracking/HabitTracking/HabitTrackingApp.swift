7455f4cd5949eab60d634aae7491f80c9e0b68f6
Dave Spina <dspina79@gmail.com>
2020-12-21 15:06:39 -0500

    Initial project creation

---
+++ HabitTracking/HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Dave Spina on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
